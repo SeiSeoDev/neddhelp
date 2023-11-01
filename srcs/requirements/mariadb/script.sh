8540ef848faa92fcc9ba577665bc231a0acd3eb7
SeiSeoDev <43590042+SeiSeoDev@users.noreply.github.com>
2023-11-01 20:34:44 +0100

    aled

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,11 +1,13 @@
-service mysql start;
-echo $(MYSQL_DATABASE)
-echo $(DOMAIN_NAME)
-
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+# service mysql start;
+# echo $(MYSQL_DATABASE)
+# echo $(DOMAIN_NAME)
+# sleep 10
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mysql -e "FLUSH PRIVILEGES;"
+# mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+# echo $(MYSQL_DATABASE)
+# echo $(DOMAIN_NAME)
 exec mysqld_safe
